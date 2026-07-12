623c2e1066e398c5c0fe4a7b6a5071082d233e0a
Carles <fores.carles@gmail.com>
2026-07-12 12:36:49 +0200

    feat: create simple color palette

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import "./styles/index.scss";
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
