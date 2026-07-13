17f5792a783667438b4945dbf738f5f9ecb4eeb8
Carles <fores.carles@gmail.com>
2026-07-13 16:08:06 +0200

    feat: sandbox

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,34 @@
 <script setup>
-
+import Card from "@/components/card.vue";
 </script>
 
 <template>
-  <div>Test</div>
+  <div class="training-log">
+    <header class="training-log__header">Header</header>
+    <main class="training-log__content">
+      <card></card>
+    </main>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.training-log {
+  display: grid;
+  grid-template-rows: 48px 1fr;
+  gap: 14px;
+  height: 100%;
+
+  &__header {
+    align-items: center;
+    background-color: var(--gray-600);
+    border-bottom: 1px solid var(--gray-500);
+    display: flex;
+    padding: 0px 16px;
+  }
+
+  &__content {
+    background-color: var(--gray-600);
+    padding: 32px 16px;
+  }
+}
+</style>
---
+++ src/components/card.vue
@@ -0,0 +1,18 @@
+<scrip setup>
+
+</scrip>
+
+<template>
+  <div class="card">
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.card {
+  background-color: var(--gray-600);
+  border: 1px solid var(--gray-500);
+  border-radius: 4px;
+  min-height: 120px;
+}
+</style>
