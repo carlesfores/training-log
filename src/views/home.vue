421f95edcf7ed6fafd6957d80620a96e8a0d2a36
Carles <fores.carles@gmail.com>
2026-07-14 11:57:17 +0200

    feat: create home view

---
+++ src/views/home.vue
@@ -0,0 +1,18 @@
+<script setup>
+import viewLayout from "@/layouts/view-layout.vue";
+
+</script>
+
+<template>
+  <view-layout>
+    <div class="home">
+      home view
+    </div>
+  </view-layout>
+</template>
+
+<style lang="scss" scoped>
+.home {
+
+}
+</style>
